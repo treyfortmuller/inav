e39617a11fe7464f72b472dfcc86009700d4761b
Trey Fortmuller <tfortmuller@mac.com>
2024-06-13 02:29:37 +0100

    successful build with the flake toolchain

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    # TODO (tff): explicitly call out nixpkgs version
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cmake # version >3.13 is required according to the docs
+          gnumake
+          ruby
+          libgcc
+
+          # Can be used as a break-glass
+          # cmake -DCOMPILER_VERSION_CHECK=OFF ..
+          gcc-arm-embedded-13 # Cmake expects 13.2.1
+        ];
+      };
+    }
+  );
+}
